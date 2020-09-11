f18bb8ce6d7d09cf12c0779c648edd1ba37afee8
Alexander <aleksandrcw@gmail.com>
2020-09-11 19:32:08 +0300

    Initial Commit

---
+++ TinkoffChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinkoffChat
+//
+//  Created by Александр Цветков on 11.09.2020.
+//  Copyright © 2020 Александр Цветков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
