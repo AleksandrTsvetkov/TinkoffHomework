35a4d97ef6c6d2154ed8ba22048273224d50bf01
Alexander <aleksandrcw@gmail.com>
2020-09-14 17:37:28 +0300

    Add logging for app and UIViewController life cycles.

--- TinkoffChat/AppDelegate.swift
+++ TinkoffChat/AppDelegate.swift
@@ -14,33 +14,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
+        #if DEBUG
+            print("Application is about to be moved from state 'not running' to state 'inactive': \(#function)")
+        #endif
         return true
     }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        #if DEBUG
+            print("Application moved from state 'not running' to state 'inactive': \(#function)")
+        #endif
         return true
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        
+        #if DEBUG
+            print("Application moved from state 'inactive' to state 'active': \(#function)")
+        #endif
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
-        
+        #if DEBUG
+            print("Application moved from state 'active' to state 'inactive': \(#function)")
+        #endif
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        
+        #if DEBUG
+            print("Application moved from state 'inactive' to state 'background': \(#function)")
+        #endif
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        
+        #if DEBUG
+        print("Application moved from state 'background' to state 'inactive': \(#function)")
+        #endif
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
-        
+        #if DEBUG
+            print("Application moved from state 'background' to state 'not running': \(#function)")
+        #endif
     }
 }
 
--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -12,30 +12,51 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        #if DEBUG
+            print("View is loaded into memory.")
+        #endif
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        #if DEBUG
+            print("View is about to be added to a view hierarchy.")
+        #endif
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        #if DEBUG
+            print("View was added to a view hierarchy.")
+        #endif
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
+        #if DEBUG
+            print("View is about to layout its subviews.")
+        #endif
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        #if DEBUG
+            print("View has just laid out its subviews.")
+        #endif
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        #if DEBUG
+            print("View is about to be removed from a view hierarchy.")
+        #endif
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
+        #if DEBUG
+            print("View was removed from a view hierarchy.")
+        #endif
     }
 }
 
